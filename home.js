6af050d9a7b019756608429cb40c037b9ec3f007
BianaryAssasin <147204449+BianaryAssasin@users.noreply.github.com>
2024-09-14 10:29:01 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,27 @@
 const news = document.getElementById("news");
+const load = document.getElementById("load");
 
+let index = 0;
 async function loadData() {
     const api = await fetch("./config.json");
     const jsonApi = await api.json();
-    const response = await fetch("https://awesome-books-e07c485b8e.strapiapp.com/api/blogs2", {
+    const response = await fetch("http://localhost:2337/api/blogs2", {
         headers: {
             "Authorization": `Bearer ${jsonApi.strapi_api.key}`
         }
     });
     const json = await response.json();
     const data = json.data;
+    if(index != 0) {
+        data.splice(0, index);
+    }
 
-    for(let item of data) {
-        const info = item.attributes;
+    for(let i = 0; i < data.length; i++) {
+        if(i >= index) {
+            index = i;
+            load.style.display = "block";
+        }
+        const info = data[i].attributes;
 
         const itemElement = document.createElement("div");
         itemElement.classList.add("item");
@@ -32,6 +41,7 @@ async function loadData() {
     }
 }
 
+load.addEventListener("click", loadData);
 loadData();
 
 function articleOpen(uuid) {
